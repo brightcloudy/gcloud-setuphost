4578d1e0da8aaa12b997ae2bb057a153f0f33d6f
Ryan Karl <ryan@brightcloud.design>
2016-07-07 03:21:48 +0000

    Improvements to main rcfiles setup script.

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # gcloud-setuphost
   - rcfiles-setup.sh
-    - Installs git
+    - installs git
     - clones [brighty-rcfiles](https://github.com/brightcloudy/brighty-rcfiles)
     - installs vim-youcompleteme
+    - clones [Vundle](https://github.com/VundleVim/Vundle.vim) to ~/.vim/bundle/Vundle.vim
     - copies .vimrc to ~
-    - runs vim setup function for vundle install
+    - runs vim setup function for vundle install using special rcfile .vimsetuprc from brighty-rcfiles
     - copies solarized colors file to ~/.vim/colors
     - copies .tmux.conf to ~
+    - installs tmux and tmate
+    - symlinks ~/.tmux.conf to ~/.tmate.conf
--- rcfiles-setup.sh
+++ rcfiles-setup.sh
@@ -3,8 +3,10 @@ cd ~
 sudo apt-get -y install git
 git clone https://github.com/brightcloudy/brighty-rcfiles
 sudo apt-get -y install vim-youcompleteme
-cp brighty-rcfiles/.vimrc ~/
-vim -c ":call SetupScript()"
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+cp ~/brighty-rcfiles/.vimrc ~/
+vim -u ~/brighty-rcfiles/.vimsetuprc -c ":call SetupScript()"
 mkdir -p ~/.vim/colors && cp ~/.vim/bundle/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
-cp brighty-rcfiles/.tmux.conf ~/
-sudo apt-get -y install tmux
+cp ~/brighty-rcfiles/.tmux.conf ~/
+sudo apt-get -y install tmux tmate
+ln -s .tmux.conf .tmate.conf
